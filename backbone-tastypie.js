dd8dec6d64b7671f14bd3de1578f6e672357d713
Viktor Nagy <viktor.nagy@toolpart.hu>
2012-07-20 13:53:11 +0200

    Merged w/ Backbone.sync from https://github.com/PaulUithol/backbone-tastypie

--- backbone-tastypie.js
+++ backbone-tastypie.js
@@ -1,6 +1,81 @@
-(function($, _, Backbone) {
+(function (root, factory) {
+  if (typeof exports === 'object') {
+
+    var jquery = require('jquery');
+    var underscore = require('underscore');
+    var backbone = require('backbone');
+
+    module.exports = factory(jquery, underscore, backbone);
+
+  } else if (typeof define === 'function' && define.amd) {
+
+    define(['jquery', 'underscore', 'backbone'], factory);
+
+  } else {
+    this.bpm = factory(jQuery, _, Backbone)
+  } 
+}(this, function($, _, Backbone) {
     Backbone.Tastypie = {
-        defaultLimit: 20
+        defaultLimit: 20,
+        doGetOnEmptyPostResponse: true,
+        doGetOnEmptyPutResponse: false,
+        apiKey: {
+            username: '',
+            key: ''
+        }
+    };
+
+    /**
+     * Override Backbone's sync function, to do a GET upon receiving a HTTP CREATED.
+     * This requires 2 requests to do a create, so you may want to use some other method in production.
+     * Modified from http://joshbohde.com/blog/backbonejs-and-django
+     */
+    Backbone.oldSync = Backbone.sync;
+    Backbone.sync = function( method, model, options ) {
+        var headers = '';
+
+        if ( Backbone.Tastypie.apiKey && Backbone.Tastypie.apiKey.username.length ) {
+            headers = _.extend( {
+                'Authorization': 'ApiKey ' + Backbone.Tastypie.apiKey.username + ':' + Backbone.Tastypie.apiKey.key
+            }, options.headers );
+            options.headers = headers;
+        }
+
+        if ( ( method === 'create' && Backbone.Tastypie.doGetOnEmptyPostResponse ) ||
+            ( method === 'update' && Backbone.Tastypie.doGetOnEmptyPutResponse ) ) {
+            var dfd = new $.Deferred();
+
+            // Set up 'success' handling
+            dfd.done( options.success );
+            options.success = function( resp, status, xhr ) {
+                // If create is successful but doesn't return a response, fire an extra GET.
+                // Otherwise, resolve the deferred (which triggers the original 'success' callbacks).
+                if ( !resp && ( xhr.status === 201 || xhr.status === 202 || xhr.status === 204 ) ) { // 201 CREATED, 202 ACCEPTED or 204 NO CONTENT; response null or empty.
+                    var location = xhr.getResponseHeader( 'Location' ) || model.id;
+                    return $.ajax( {
+                           url: location,
+                           headers: headers,
+                           success: dfd.resolve,
+                           error: dfd.reject
+                        });
+                }
+                else {
+                    return dfd.resolveWith( options.context || options, [ resp, status, xhr ] );
+                }
+            };
+
+            // Set up 'error' handling
+            dfd.fail( options.error );
+            options.error = function( xhr, status, resp ) {
+                dfd.rejectWith( options.context || options, [ xhr, status, resp ] );
+            };
+
+            // Make the request, make it accessibly by assigning it to the 'request' property on the deferred
+            dfd.request = Backbone.oldSync( method, model, options );
+            return dfd;
+        }
+
+        return Backbone.oldSync( method, model, options );
     };
 
     _.extend(Backbone.Model.prototype, {
@@ -101,4 +176,4 @@
     var urlError = function() {
         throw new Error('A "url" property or function must be specified');
     };
-})(window.$, window._, window.Backbone);
+}));
--- test/tests.js
+++ test/tests.js
@@ -8,9 +8,22 @@ var test_users = _([
     {id:7, username:"azat", resource_uri:'/api/v1/user/7/'}
 ]);
 
+function mock_create_user(request) {
+    test_users.push({id:8, username:'zolivar', resource_uri:'/api/v1/user/8/'})
+    return {
+        status: 201,
+        headers: {Location: '/api/v1/user/8/'},
+        success: true,
+        responseText: null
+    };    
+}
+
 $.mockjax(function(request){
+    if(request.type == 'POST') 
+        return mock_create_user(request)
+
     var userDetail = request.url.match(/\/api\/v1\/user\/(\d+)\/$/i);
- 
+     
     if (userDetail) {
         var userId = userDetail[1];
 
@@ -142,3 +155,12 @@ test("model meta", function() {
 
     notEqual(foo.meta.offset, bar.meta.offset);
 });
+
+asyncTest("testing model creation", 6, function(){
+  var users = new Users();  
+  users.create({'username': 'zolivar'}, {success: function(user, response){
+    equal(response.username, 'zolivar')
+    equal(response.id, 8)
+    equal(response.resource_uri, '/api/v1/user/7/')
+  }})
+})
